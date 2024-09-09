80419a1cf64dd12a40bf58157a1ba95cd9828b6c
Tom Webber <t.c.w.webber@gmail.com>
2024-09-09 20:21:50 +0200

    add: actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    clippy::style,
+    clippy::perf,
+    clippy::complexity,
+    clippy::correctness,
+    clippy::restriction,
+    clippy::nursery,
+    // clippy::cargo
+)]
+#![feature(stmt_expr_attributes)]
+#![allow(clippy::blanket_clippy_restriction_lints)]
+#![allow(clippy::implicit_return)]
+#![allow(clippy::single_call_fn)]
+#![allow(clippy::string_add)]
+#![allow(clippy::missing_docs_in_private_items)]
+#![allow(clippy::question_mark_used)]
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::io;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
